5098cd6e28bfde83de253e4b253e1c59f3f24ea6
DropDemBits <ring0hicc@gmail.com>
2019-09-28 22:15:46 -0400

    Change water spread - Only spreads laterally if there is a solid block below

--- src/ddb/io/voxelnet/block/BlockWater.java
+++ src/ddb/io/voxelnet/block/BlockWater.java
@@ -25,21 +25,24 @@ public class BlockWater extends Block
 	public void onTick(World world, int x, int y, int z)
 	{
 		// Check surronding blocks
-		final Facing[] dirs = new Facing[] { Facing.NORTH, Facing.WEST, Facing.SOUTH, Facing.EAST, Facing.DOWN, };
+		final Facing[] dirs = new Facing[] { Facing.DOWN, Facing.NORTH, Facing.WEST, Facing.SOUTH, Facing.EAST, };
 		for (Facing facing : dirs)
 		{
 			int blockX = x + facing.getOffsetX();
 			int blockY = y + facing.getOffsetY();
 			int blockZ = z + facing.getOffsetZ();
 			
-			if (!world.isChunkPresent(blockX >> 4, blockY >> 4, blockZ >> 4)) //world.getChunk(blockX >> 4, blockY >> 4, blockZ >> 4) == world.EMPTY_CHUNK)
+			if (!world.isChunkPresent(blockX >> 4, blockY >> 4, blockZ >> 4))
 				continue;
 			
 			Block block = Block.idToBlock(world.getBlock(blockX, blockY, blockZ));
 			
 			Vec3i newPlace = new Vec3i(blockX, blockY, blockZ);
-			if (block == Blocks.AIR && !availableBlocks.contains(newPlace))
+			if ((block == Blocks.AIR || block.canBeReplacedBy(world, this, blockX, blockY, blockZ)) && !availableBlocks.contains(newPlace))
 				availableBlocks.push(newPlace);
+			
+			if (facing == Facing.DOWN && (block == Blocks.AIR || block == Blocks.WATER))
+				break;
 		}
 	}
 	
@@ -55,14 +58,14 @@ public class BlockWater extends Block
 	@Override
 	public boolean canBeReplacedBy(World world, Block block, int x, int y, int z)
 	{
-		// Water can be replaced by anything
-		return true;
+		// Water can be replaced by anything but water
+		return block != Blocks.WATER;
 	}
 	
 	@Override
 	public RenderLayer getRenderLayer()
 	{
-		return RenderLayer.TRANSPARENT;
+		return RenderLayer.FLUID;
 	}
 	
 }
