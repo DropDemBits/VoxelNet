d2cd8df012d40c08ec4af44e6cd86a337cca457e
DropDemBits <ring0hicc@gmail.com>
2020-05-18 21:31:15 -0400

    Make grass spread under translucent, transparent blocks

--- src/ddb/io/voxelnet/block/BlockDirt.java
+++ src/ddb/io/voxelnet/block/BlockDirt.java
@@ -1,8 +1,5 @@
 package ddb.io.voxelnet.block;
 
-import ddb.io.voxelnet.util.Facing;
-import ddb.io.voxelnet.world.World;
-
 public class BlockDirt extends Block
 {
 	
@@ -10,36 +7,6 @@ public class BlockDirt extends Block
 	{
 		setSolid(true);
 		setFaceTextures(new int[] {2, 2, 2, 2, 2, 2});
-	}
-	
-	@Override
-	public boolean isRandomlyTickable()
-	{
-		return true;
-	}
-	
-	@Override
-	public void onRandomTick(World world, int x, int y, int z)
-	{
-		// 1/4 chance of receiving a spread
-		if (world.worldRandom.nextFloat() > 0.25
-				|| world.getBlock(x, y + 1, z) != Blocks.AIR)
-			return;
-		
-		for (Facing dir : Facing.CARDINAL_FACES)
-		{
-			if (world.getBlock(x + dir.getOffsetX(), y + dir.getOffsetY(), z + dir.getOffsetZ()) == Blocks.GRASS)
-			{
-				// Spread randomly
-				if (world.getBlock(x + dir.getOffsetX(), y + dir.getOffsetY() + 1, z + dir.getOffsetZ()) == Blocks.AIR)
-				{
-					world.setBlock(x, y, z, Blocks.GRASS);
-					
-					// Only spread once
-					break;
-				}
-			}
-		}
 	}
 	
 }
--- src/ddb/io/voxelnet/block/BlockGrass.java
+++ src/ddb/io/voxelnet/block/BlockGrass.java
@@ -1,5 +1,6 @@
 package ddb.io.voxelnet.block;
 
+import ddb.io.voxelnet.util.Facing;
 import ddb.io.voxelnet.world.World;
 
 public class BlockGrass extends Block
@@ -19,10 +20,35 @@ public class BlockGrass extends Block
 	@Override
 	public void onRandomTick(World world, int x, int y, int z)
 	{
-		if (world.getBlock(x, y + 1, z) != Blocks.AIR)
+		if (!canGrowUnder(world.getBlock(x, y + 1, z)))
 		{
 			// Bye grass
 			world.setBlock(x, y, z, Blocks.DIRT);
+			return;
 		}
+		
+		// 1/4 chance of doing a spread
+		if (world.worldRandom.nextFloat() > 0.25)
+			return;
+		
+		for (Facing dir : Facing.CARDINAL_FACES)
+		{
+			if (world.getBlock(x + dir.getOffsetX(), y + dir.getOffsetY(), z + dir.getOffsetZ()) == Blocks.DIRT)
+			{
+				if (canGrowUnder(world.getBlock(x + dir.getOffsetX(), y + dir.getOffsetY() + 1, z + dir.getOffsetZ())))
+				{
+					// Found dirt block, spread to it
+					world.setBlock(x + dir.getOffsetX(), y + dir.getOffsetY(), z + dir.getOffsetZ(), Blocks.GRASS);
+					
+					// Only spread once
+					break;
+				}
+			}
+		}
+	}
+	
+	public static boolean canGrowUnder(Block block)
+	{
+		return block.isTransparent() && block.getOpacity() == 0;
 	}
 }
